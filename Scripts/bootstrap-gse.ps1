24fc2329a03828599cc2cd2e42c777b6a9a9cf8a
dsf-internal <85113687+dsf-internal@users.noreply.github.com>
2021-05-31 19:20:53 +0300

    Update bootstrap-gse.ps1

--- Scripts/bootstrap-gse.ps1
+++ Scripts/bootstrap-gse.ps1
@@ -5,15 +5,5 @@ $ShortcutFile = "$env:Public\Desktop\ProvisionRobot.lnk"
 $WScriptShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
 $Shortcut.TargetPath = $TargetFile
-$Shortcut.Arguments = " /C C:\Temp\ModernRobotProvisioning.exe"
+$Shortcut.Arguments = " /C C:\Temp\folderSelector.exe"
 $Shortcut.Save()
-
-
-$TargetFile = "$env:SystemRoot\System32\cmd.exe"
-$ShortcutFile = "$env:Public\Desktop\DeprovisionRobot.lnk"
-$WScriptShell = New-Object -ComObject WScript.Shell
-$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
-$Shortcut.TargetPath = $TargetFile
-$Shortcut.Arguments = " /C C:\Temp\ModernRobotProvisioning.exe -dp"
-$Shortcut.Save()
-
