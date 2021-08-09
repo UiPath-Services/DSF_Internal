dda5ad605b1ab82f9923eb15b18c0ae4641774a7
mihaiandreiuipath <78545063+mihaiandreiuipath@users.noreply.github.com>
2021-08-09 21:12:05 +0300

    Update bootstrap-gse.ps1

--- Scripts/bootstrap-gse.ps1
+++ Scripts/bootstrap-gse.ps1
@@ -1,22 +1,13 @@
 Add-MpPreference -ExclusionPath "C:\Temp"
 
 $TargetFile = "$env:SystemRoot\System32\cmd.exe"
-$ShortcutFile = "$env:Public\Desktop\ProvisionRobot.lnk"
+$ShortcutFile = "$env:Public\Desktop\Cloud Provision.exe"
 $WScriptShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
 $Shortcut.TargetPath = $TargetFile
-$Shortcut.Arguments = " /C C:\Temp\folderSelector.exe"
+$Shortcut.Arguments = " /C C:\Temp\Cloud Folder Selector UI Provision Setup 1.0.0.exe"
 $Shortcut.Save()
 
 try{
   & "C:\Temp\sap.ps1"
 }catch{ Write-Host "script not found"}
-
-try{
-  & "C:\Temp\InstallUiPathRobot.ps1"
-}catch{ Write-Host "script not found"}
-
-try{
-  & "C:\Temp\git.ps1"
-}catch{ Write-Host "script not found"}
-finally { Remove-Item -Path C:\Temp\git.ps1 -Force }
