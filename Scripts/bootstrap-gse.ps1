43c212e6b36ab24799074b190b25c882d177306a
mihaiandreiuipath <78545063+mihaiandreiuipath@users.noreply.github.com>
2021-12-16 10:05:51 +0200

    Update bootstrap-gse.ps1

--- Scripts/bootstrap-gse.ps1
+++ Scripts/bootstrap-gse.ps1
@@ -12,15 +12,6 @@ $ "C:\Temp\dsf-provisioner-1.0.0 Setup.exe"
 
 
 try{
-$TargetFile = "C:\Temp\Staging External - Folder Selector UI Provision Setup 1.0.0"
-$ShortcutFile = "$env:Public\Desktop\Staging External - Robot.lnk"
-$WScriptShell = New-Object -ComObject WScript.Shell
-$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
-$Shortcut.TargetPath = $TargetFile
-$Shortcut.Save()
-}catch{ Write-Host "script not found"}
-
-try{
 $TargetFile = "C:\Program Files (x86)\Postman\Postman.exe"
 $ShortcutFile = "$env:Public\Desktop\Postman.lnk"
 $WScriptShell = New-Object -ComObject WScript.Shell
