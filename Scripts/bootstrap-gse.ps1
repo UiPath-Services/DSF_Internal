cb5da98aab30f118fcfc06e3df3160af447e0d5d
mihaiandreiuipath <78545063+mihaiandreiuipath@users.noreply.github.com>
2021-12-16 09:47:49 +0200

    Update bootstrap-gse.ps1

--- Scripts/bootstrap-gse.ps1
+++ Scripts/bootstrap-gse.ps1
@@ -1,23 +1,14 @@
 Add-MpPreference -ExclusionPath "C:\Temp"
 
 try{
-$TargetFile = "C:\Temp\Cloud Folder Selector UI Provision Setup 1.0.0.exe"
-$ShortcutFile = "$env:Public\Desktop\Cloud Provision.lnk"
-$WScriptShell = New-Object -ComObject WScript.Shell
-$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
-$Shortcut.TargetPath = $TargetFile
-$Shortcut.Save()
-}catch{ Write-Host "script not found"}
-
-
-try{
-$TargetFile = "C:\Temp\Staging Internal Folder Selector UI Provision Setup 1.0.0.exe"
+$TargetFile = "C:\Temp\dsf-provisioner-1.0.0 Setup.exe"
 $ShortcutFile = "$env:Public\Desktop\Staging Provision.lnk"
 $WScriptShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
 $Shortcut.TargetPath = $TargetFile
 $Shortcut.Save()
 }catch{ Write-Host "script not found"}
+
 
 try{
 $TargetFile = "C:\Temp\Staging External - Folder Selector UI Provision Setup 1.0.0"
