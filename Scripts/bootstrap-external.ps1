cb6ea29a9d77147bf667171a8f7c012c0fc263bf
mihaiandreiuipath <78545063+mihaiandreiuipath@users.noreply.github.com>
2022-03-09 14:27:39 +0200

    Update bootstrap-external.ps1

--- Scripts/bootstrap-external.ps1
+++ Scripts/bootstrap-external.ps1
@@ -1,12 +1,13 @@
 Add-MpPreference -ExclusionPath "C:\Temp"
 
 try{
-$TargetFile = "C:\Temp\Staging External - Folder Selector UI Provision Setup 1.0.0.exe"
-$ShortcutFile = "$env:Public\Desktop\Staging External - Robot.lnk"
+$TargetFile = "C:\Temp\dsf-provisioner-1.0.0 Setup.exe"
+$ShortcutFile = "$env:Public\Desktop\Staging Provision.lnk"
 $WScriptShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
 $Shortcut.TargetPath = $TargetFile
 $Shortcut.Save()
+$ "C:\Temp\dsf-provisioner-1.0.0 Setup.exe"
 }catch{ Write-Host "script not found"}
 
 
